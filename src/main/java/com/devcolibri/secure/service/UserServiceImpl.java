d5872d4314823f9973bde314b0b80e28488a1cd7
Panineduard <veselaya_gora@mail.ru>
2015-12-27 18:10:26 +0200

    Initial commit

---
+++ src/main/java/com/devcolibri/secure/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.devcolibri.secure.service;
+import com.devcolibri.secure.entity.User;
+import org.springframework.stereotype.Service;
+@Service
+public class UserServiceImpl  {
+
+    public User getUser(String login) {
+        User user = new User();
+
+
+        return user;
+    }
+
+}
